dff51b24f03285d7123493e9076278ec5ecbe43b
Meenesh Chaudhary <meeneshc017@gmail.com>
2025-12-24 22:17:01 +0530

    added js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ function setTime() {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    if (hours < 10) hours = '0' + hours;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+    timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
 
 }
 setInterval(setTime, 1000);
