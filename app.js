257d329b838de224d0138300137e9e92ea90136d
Meenesh Chaudhary <meeneshc017@gmail.com>
2025-12-27 21:41:53 +0530

    updated js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function setTime() {
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     let hours = now.getHours();
     let minutes = now.getMinutes();
-    if (hours < 10) hours = '0' + hours;
-    if (minutes < 10) minutes = '0' + minutes;
-    if (seconds < 10) seconds = '0' + seconds;
+    if (hours < 0) hours = '0' + hours;
+    if (minutes < 0) minutes = '0' + minutes;
+    if (seconds < 0) seconds = '0' + seconds;
     timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
 
 }
